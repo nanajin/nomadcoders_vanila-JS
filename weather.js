9c29b74a3221ce69732d93ec53427d0f9e092177
nanajin <nmj5182@gmail.com>
2022-09-16 16:49:05 +0900

    made chrome, weather

---
+++ weather.js
@@ -0,0 +1,9 @@
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    console.log(lat, long);
+}
+function onGeoError(){
+    alert("Can't find your location.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
